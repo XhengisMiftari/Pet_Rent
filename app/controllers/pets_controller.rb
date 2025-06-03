b3e43ed4df29b5304c972f3bca7b2a926b4fcf95
sartoscht loock <sartoscht.loock@gmail.com>
2025-06-03 16:15:14 +0200

    new page with create form to create pets

---
+++ app/controllers/pets_controller.rb
@@ -0,0 +1,20 @@
+class PetsController < ApplicationController
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = current_user
+
+    if @pet.save
+      redirect_to root_path, notice: 'Pet was born. Congratulations!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  private
+  def pet_params
+    params.require(:pet).permit(:species, :name, :price, :location, :availability)
+  end
+end
