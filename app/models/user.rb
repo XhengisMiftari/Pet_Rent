972002f49581f2a26a3a2c2a329fbeb3bd2ed570
Roger Tsikata <roger.tsikata@gmail.com>
2025-06-03 15:03:35 +0100

    index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :pets, :reservations
+  has_many :pets
+  has_many :reservations
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
