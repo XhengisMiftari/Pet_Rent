0def1b6c39b8f773c75e6b4ae196ca6edd3d205b
xhengis miftari <xhengismiftari@gmail.com>
2025-06-05 12:14:22 +0200

    Map geocoding

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_06_03_211754) do
+ActiveRecord::Schema[7.1].define(version: 2025_06_05_090834) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_03_211754) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.string "image_url"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
